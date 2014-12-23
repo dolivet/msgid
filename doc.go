9c482239f4fe6b6beda8928ca54d263ed3bdc955
David Olivet <davidolivet@gmail.com>
2014-12-23 15:13:06 -0800

    More doc cleanup.

--- doc.go
+++ doc.go
@@ -1,17 +1,17 @@
 /*
 Package msgid allows for creating a unique key traceable to a particular msgid
 generator or instance.  The msgid is a composite of a time (to millisecond
-resolution), a spawn (or node) id and an arbitrary sequence/counter value.
+resolution), a spawner (or node) id and an arbitrary sequence/counter value.
 
 
 Usage
 
 Create a MsgIdGenerator with a unique (to that instance)  spawner id.  Spawner
 Ids would have to be coordinated across multiple instances. Millis are stored
-in an 64 bit integer. A 32 bit spawn id and sequence are stored in 64 bit
+in a 64 bit integer. A 32 bit spawn id and sequence value are stored in 64 bit
 integer.
 
-For convenience "NextKey" will return a base64 encoded string that can
+For convenience, "NextToken" will return a base64 encoded string that can
 be decoded to a MsgId instance.
 
 */
